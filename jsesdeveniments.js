812fdce702e78c028dc30e49c472d5ce92dded64
ivan02 <ivan.esteban@institutvidreres.cat>
2021-12-15 11:52:50 +0100

    Ex3-Esdeveniments

--- jsesdeveniments.js
+++ jsesdeveniments.js
@@ -1,8 +1,18 @@
 document.onkeydown=function(tecla){
        document.getElementById('hola').innerHTML=tecla.key
+       var guardarLletra = document.getElementById('lletres')
+       guardarLletra.innerHTML += tecla.key + ' , '
+       
+       var reiniciar = document.getElementById("reset")
+       reiniciar.onclick = function(){
+       guardarLletra.innerHTML=''
+} 
+       
 }
 
 document.onkeyup=function(){
        document.getElementById('hola').innerHTML=""
 }
-   
+
+
+
